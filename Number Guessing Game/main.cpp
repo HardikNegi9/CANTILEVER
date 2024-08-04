0f901b35d2d2bb5c8246ca710b0d7f1d05d09747
HardikNegi9 <hardiknegi764isc@gmail.com>
2024-08-04 11:39:40 +0530

    Number Guessing Game Conditional

--- Number Guessing Game/main.cpp
+++ Number Guessing Game/main.cpp
@@ -8,24 +8,30 @@ int main() {
     int num_to_num_guessed = rand() % 100 + 1;
     int num_guessed = 0;
     int attempts = 0;
+    char running = 'y';
 
 
     cout << "Welcome to the Number Guess Game!" << endl;
     cout << "I have generated a number between 1 and 100. Try to Guess it!" << endl;
 
-    while (true){
+    do{
         cout << "Enter your Guess: ";
         cin >> num_guessed;
+        
         attempts++;
         if(num_guessed == num_to_num_guessed){
             cout << "Congratulations! You Guess the number in " << attempts << " attempts." << endl;
-            break;
+            running = 'n'; 
         }else if (num_guessed < num_to_num_guessed){
             cout << "Guess Higher" << endl;
         } else{
             cout << "Guess Lower" << endl;
         }
-    }
+        if(running == 'n'){
+            cout << "Do you want to play again? (y/n): ";
+            cin >> running;
+        }
+    }while(running == 'y' || running == 'Y');
 
     return 0;
 }
