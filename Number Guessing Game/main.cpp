602e73327465a8b05eed8b9a58fa36a74c78f441
HardikNegi9 <hardiknegi764isc@gmail.com>
2024-08-04 11:55:32 +0530

    Number Guessing Game Conditional Correction

--- Number Guessing Game/main.cpp
+++ Number Guessing Game/main.cpp
@@ -4,33 +4,36 @@
 using namespace std;
 
 int main() {
-    srand(time(NULL));
-    int num_to_num_guessed = rand() % 100 + 1;
     int num_guessed = 0;
-    int attempts = 0;
-    char running = 'y';
-
+    int attempts = 0; // no of attemps to guess the no.
+    char running = 'y'; // state of game -> running/stop.
 
     cout << "Welcome to the Number Guess Game!" << endl;
     cout << "I have generated a number between 1 and 100. Try to Guess it!" << endl;
 
+    // Game loop
     do{
-        cout << "Enter your Guess: ";
-        cin >> num_guessed;
-        
-        attempts++;
-        if(num_guessed == num_to_num_guessed){
-            cout << "Congratulations! You Guess the number in " << attempts << " attempts." << endl;
-            running = 'n'; 
-        }else if (num_guessed < num_to_num_guessed){
-            cout << "Guess Higher" << endl;
-        } else{
-            cout << "Guess Lower" << endl;
+        srand(time(NULL));
+        int num_to_guess = rand() % 100 + 1; // generating a number between 1 - 100.
+
+        while(true){
+            cout << "Enter your Guess: ";
+            cin >> num_guessed;
+            attempts++;
+            if(num_guessed == num_to_guess){
+                cout << "Congratulations! You Guess the number in " << attempts << " attempts." << endl;
+                attempts = 0;
+                break;
+            }else if (num_guessed < num_to_guess){
+                cout << "Guess Higher" << endl;
+            } else{
+                cout << "Guess Lower" << endl;
+            }
         }
-        if(running == 'n'){
-            cout << "Do you want to play again? (y/n): ";
-            cin >> running;
-        }
+
+        // checking that if the player wants to play again after guessing the right no.
+        cout << "Do you want to play again? (y/n): ";
+        cin >> running;
     }while(running == 'y' || running == 'Y');
 
     return 0;
